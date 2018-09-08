a7c6a7cb44334636a02deadc7535d030a052cdcc
Lucas Lafarga <elrutas@gmail.com>
2018-09-08 16:48:43 +0200

    Initial commit with desired folder structure and one activity

---
+++ app/src/main/java/com/example/lucas/spidey2/ui/features/comiclist/ComicListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.lucas.spidey2.ui.features.comiclist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lucas.spidey2.R
+
+class ComicListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comic_list)
+    }
+}
